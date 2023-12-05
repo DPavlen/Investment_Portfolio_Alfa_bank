cd60faf2a99c0cae3550d8b611535488278b1b1f
Pavlen <pavlen@192.168.1.48>
2023-12-05 14:33:59 +0300

    3st lesson. Валидация данных с Pydantic

--- main.py
+++ main.py
@@ -1,20 +1,67 @@
-from fastapi import FastAPI
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from pydantic import BaseModel, Field
+
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+# from fastapi.exceptions import ValidationError
+from fastapi.exceptions import ValidationException
+from fastapi.responses import JSONResponse
 
 # Далее создаем приложение, которое явл-ся экземпляром FastAPI,  зададим ему понятное имя
 app = FastAPI(
     title="Traiding App"
 )
 
+# Благодаря этой функции клиент видит ошибки, происходящие на сервере, вместо 500 "Internal server error"
+@app.exception_handler(ValidationException)
+async def validation_exception_handler(request: Request, exc: ValidationException):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors()}),
+    )
+
 # База данных пользователей. Соответсвенно у каждого есть id
 fake_users = [
-    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 1, "role": "admin", "name": ["Bob"]},
     {"id": 2, "role": "investor", "name": "John"},
     {"id": 3, "role": "trader", "name": "Matt"},
+    {"id": 4, "role": "investor", "name": "Homer", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
+    ]},
 ]
+
+class DegreeType(Enum):
+    """Валидация для квалификации типа степени трейдера (Degree Type)."""
+    newbie = "newbie"
+    expert = "expert"
+    managing_director = "managing_director"
+
+
+class Degree(BaseModel):
+    """Модель Degree для модели User поля degree, где будет список экспертов."""
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    """Модель списка юзеров.
+    Добавим поле degree c отдельной описанной структурой данных Degree.
+    Optional указывает, что атрибут degree может принимать значение None
+    или тип данных, указанный после него список.
+    """
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = [ ]
+
 
 # Далее создаем точку входа, для получения данных о пользователе # Оборачиваем в фигурные скобки {user_id} и далее в функции 
 # get_user получаем параметр user_id и необходимо получить # конкретного пользователя с list_comprehesions
-@app.get("/users/{user_id}")
+# Модель данных, которой мы отвечаем с помощью response_model
+@app.get("/users/{user_id}", response_model=List[User])
 def get_user(user_id: int):
     """Получаем по id текущего usera."""
     return [user for user in fake_users if user.get("id") == user_id]
@@ -26,26 +73,21 @@ fake_trades = [
 ]
 
 
-@app.get("/trades")
-def get_trades(limit: int = 1, offset: int = 0):
-    """Параметры: limit-количество получаемых сделок
-    offset-сдвиг(например если хотим получить 2-ю, то надо 1)."""
-    return fake_trades[offset:][:limit]
+class Trade(BaseModel):
+    """Модель Трейда данных для валидации. Наследуемся от базовой BaseModel.
+    id - id трейда, user_id - id usera, currency - валюта, 
+    side - сторона, price - стоимость, amount - количество.
+    """
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
 
 
-# База данных пользователей fake_users2.
-fake_users2 = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-]
-
-@app.post("/users/{user_id}")
-def change_user_name(user_id: int, new_name: str):
-    """Итератор filter, с анонимной функцией lambda и итерируемый объект 
-    список словарей fake_users2 и берем 0(т.е. id=1). Далее итератор оборачиваем в list."""
-    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
-    # Далее пользователю сменим name.
-    current_user["name"] = new_name
-    # Далее по стандарту возвращаем HTTP код и данные
-    return {"staus": 200, "data": current_user}
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    """Расширяем список сделок, нашими новыми trade."""
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
